7bb9b33e04ec385deb98b094fd42d492223274b7
十三月 <lunarjan@shisanyuedeMacBook-Pro.local>
2021-12-05 16:27:22 +0800

    docs: 修改文档

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin() // 在开发或者构建的过程中把eslint集成在vite里
+  ]
 })
