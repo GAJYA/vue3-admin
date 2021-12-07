5e049ce567958d6b53e8652715ed1b658bf679af
十三月 <lunarjan@shisanyuedeMacBook-Pro.local>
2021-12-07 18:05:37 +0800

    chore: using vite server.proxy to resolve cross-domain problems

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,26 @@ export default defineConfig({
         additionalData: '@import "@/styles/variables.scss";' // 最终是把这个字符串注入到css模块
       }
     }
+  },
+  server: {
+    proxy: {
+      // 字符串简写写法
+      // 以/foo/123开头的地址都被代理到http://localhost:4567/foo/123
+      // '/foo': 'http://localhost:4567',
+      // 选项写法
+      // '/api': {
+      '/admin': {
+        // target: 'http://jsonplaceholder.typicode.com', // 代理的目标地址
+        target: 'https://shop.fed.lagou.com/api/', // 代理的目标地址
+        // 兼容基于名字的虚拟主机
+        // 通过http请求头部的origin字段可以获取到域名
+        // 在开发阶段，默认的origin是：localhost:3000，服务端可能无法处理
+        // changeOrigin: true，代理服务会把origin修改为设置的目标地址`http://jsonplaceholder.typicode.com`
+        changeOrigin: true
+        // 路径重写
+        // 真正的接口地址不存在api的时候，需要把api去掉，此处的api只是方便做规则校验而设置的
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
