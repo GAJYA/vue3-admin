cb981940393a4ccea7b5710f5048a59188baa568
十三月 <lunarjan@shisanyuedeMacBook-Pro.local>
2021-12-10 12:48:15 +0800

    feat: token eapired

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import { store } from '@/store/index'
+import { useRouter } from 'vue-router'
 console.log(import.meta.env)
 
 // const store = useStore()
+const router = useRouter()
 const request = axios.create({
   // 不同环境可能有不同地址
   baseURL: import.meta.env.VITE_API_BASEURL // 基本路径
@@ -27,17 +29,42 @@ request.interceptors.request.use(
   }
 )
 
+// 控制登录过期的锁，比如当前页面多个请求都需要权限，可能会出现连续弹出框的情况
+let isRefresh = false
 // 响应拦截器
 request.interceptors.response.use(
   response => {
+    const status = response.data.status
+    // 正确的情况
+    if (!status || status === 200) {
+      return response
+    }
     // 统一处理响应错误，例如 token 无效、服务端异常等
-    // 服务使用的自定义状态码的情况
-    if (response.data.status && response.data.status !== 200) {
-      ElMessage.error(response.data.msg || '请求失败，请稍后重试')
-      // 手动返回一个Promise异常
+    if (status === 410000) {
+      if (isRefresh) return
+      isRefresh = true
+      ElMessageBox.confirm('您的登录已过期，请确认重新登录，或取消停留在当前页面', '登录过期', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消'
+      }).then(() => {
+        // 本地清除过期的登录状态
+        store.commit('setUser', null)
+        // 跳转到登录页面
+        router.push({
+          name: 'login',
+          query: { redirect: router.currentRoute.value.fullPath }
+        })
+      // 抛出异常
+      }).finally(() => {
+        // 确保本次完了以后，后续可以正常处理
+        isRefresh = false
+      })
       return Promise.reject(response.data)
     }
-    return response
+    // 服务使用的自定义状态码的情况，接口返回的失败的状态码
+    ElMessage.error(response.data.msg || '请求失败，请稍后重试')
+    // 手动返回一个Promise异常
+    return Promise.reject(response.data)
   },
   err => {
     return Promise.reject(err)
