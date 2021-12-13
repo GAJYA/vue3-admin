9751521a0e9299516b4b8072026a9610607c7cf0
十三月 <lunarjan@shisanyuedeMacBook-Pro.local>
2021-12-13 15:25:04 +0800

    refactor: props and emits add ts types

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -11,26 +11,55 @@
 </template>
 
 <script lang="ts" setup>
-const props = defineProps({
-  page: {
-    type: Number,
-    default: 1
-  },
-  limit: {
-    type: Number,
-    default: 10
-  },
-  listCount: {
-    type: Number,
-    default: 0
-  },
-  loadList: {
-    type: Function,
-    default: () => {}
-  }
-})
+// 原生类型可以使用PropType来指定复杂类型
+// import { PropType } from '@vue/runtime-core'
+// const props = defineProps({
+//   page: {
+//     type: Number,
+//     default: 1
+//   },
+//   limit: {
+//     type: Number,
+//     default: 10
+//   },
+//   listCount: {
+//     type: Number,
+//     default: 0
+//   },
+//   loadList: {
+//     type: Function,
+//     default: () => {}
+//   },
+//   obj: {
+//     type: Object as PropType<{ a: string, b: number }>,
+//     required: true
+//   }
+// })
 
-const emit = defineEmits(['update:page', 'update:limit'])
+// 定义接口
+interface PropsType {
+    page: number
+    limit: number
+    listCount: number
+    loadList: () => void
+}
+// 使用withDefaults方法来指定默认值
+const props = withDefaults(defineProps<PropsType>(), {
+  page: 1,
+  limit: 10,
+  listCount: 0,
+  loadList: () => {}
+})
+// 没有默认值，只指定类型
+// const props = defineProps<PropsType>()
+
+interface EmitType {
+    // (e: 'update:page', page: number, a: string) : void // 多个参数时候
+    (e: 'update:page', page: number) : void
+    (e: 'update:limit', limit: number) : void
+}
+const emit = defineEmits<EmitType>()
+// const emit = defineEmits(['update:page', 'update:limit'])
 
 const handleSizeChange = (size: number) => {
   emit('update:page', 1)
